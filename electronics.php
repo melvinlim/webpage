6417006be3b816f7ecb5ac9115e9bd8b40a6e493
Melvin Lim <lim.dongxing@gmail.com>
2015-08-29 10:48:21 -0700

    sidebar navigation

--- electronics.php
+++ electronics.php
@@ -2,8 +2,14 @@
 <body style="background-color:black" style="color:white">
 <?php
 	require('nav.html');
-	require('subnav-electronics.html');
+	echo '<table>';
+	echo '<th width="75%">';
 	require('electronics.html');
+	echo '</th>';
+	echo '<th width="25%">';
+	require('subnav-electronics.html');
+	echo '</th>';
+	echo '</table>';
 ?>
 </body>
 </html>
