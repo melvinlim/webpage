1d8d9305b43f74d0ef503394f1003337f25bbfe4
Melvin Lim <lim.dongxing@gmail.com>
2015-08-29 22:46:40 -0700

    added subpage

---
+++ elec-1.php
@@ -0,0 +1,15 @@
+<html>
+<body style="background-color:black" style="color:white">
+<?php
+	require('nav.html');
+	echo '<table>';
+	echo '<td width="75%">';
+	require('elec-1.html');
+	echo '</td>';
+	echo '<td valign="top" width="25%">';
+	require('subnav-electronics.html');
+	echo '</td>';
+	echo '</table>';
+?>
+</body>
+</html>
