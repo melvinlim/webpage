01ef5b7b1dca5b60f8d18740453b21c72f6c66a4
Melvin Lim <lim.dongxing@gmail.com>
2015-09-27 10:44:56 -0700

    uhf antenna

---
+++ antennas.php
@@ -0,0 +1,15 @@
+<html>
+<body style="background-color:black" style="color:white">
+<?php
+	require('nav.html');
+	echo '<table>';
+	echo '<td width="75%">';
+	require('antennas.html');
+	echo '</td>';
+	echo '<td valign="top" width="25%">';
+	require('subnav-antennas.html');
+	echo '</td>';
+	echo '</table>';
+?>
+</body>
+</html>
